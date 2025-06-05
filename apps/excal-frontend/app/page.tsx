10a0aac28f10cb37413dd5c7757fe1876c3a9c4e
Aadit <aadits2004@gmail.com>
2025-06-05 16:59:01 +0000

    navbar

--- apps/excal-frontend/app/page.tsx
+++ apps/excal-frontend/app/page.tsx
@@ -1,8 +1,21 @@
+"use client"
+
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      excal fe
+    <div className="fixed top-0 left-0 bg-black/70 border-b backdrop-blur-md w-screen h-18">
+      <div className="flex justify-between pt-2">
+        <div className="flex justify-center items-center text-3xl pl-4 pt-2">Excal</div>
+        <div className="flex justify-center items-center pt-3">
+          <div className="px-4 hover:underline hover:underline-offset-3 transition-all duration-300 hover:-translate-y-1 cursor-pointer">Features</div>
+          <div className="px-4 hover:underline hover:underline-offset-3 transition-all duration-300 hover:-translate-y-1 cursor-pointer">How it works</div>
+          <div className="px-4 hover:underline hover:underline-offset-3 transition-all duration-300 hover:-translate-y-1 cursor-pointer">Testimonials</div>
+          <div className="px-4 hover:underline hover:underline-offset-3 transition-all duration-300 hover:-translate-y-1 cursor-pointer">Pricing</div>
+        </div>
+      </div>
+
     </div>
   );
 }
+
