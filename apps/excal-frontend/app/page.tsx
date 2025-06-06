06e02b39b2d4c0bb7f2f64714299c7729c50d1c8
Aadit <aadits2004@gmail.com>
2025-06-06 12:21:33 +0000

    features section

--- apps/excal-frontend/app/page.tsx
+++ apps/excal-frontend/app/page.tsx
@@ -2,12 +2,30 @@
 
 import Link from "next/link"
 import Image from "next/image"
-import { ArrowRight } from 'lucide-react'
-import { Users } from 'lucide-react'
+import { ArrowRight, Share2, Users, Download, Layers } from 'lucide-react'
+import { motion } from "framer-motion"
 
 export default function Home() {
+  const features = [
+    {
+      icon: <Users className="h-10 w-10" />,
+      title: "Real-time collboration",
+      description: "Work together with your team on the same canvas in real-time, seeing changes as they happen."
+    },
+    {
+      icon: <Share2 className="h-10 w-10" />,
+      title: "Easy Sharing",
+      description: "Share your drawings with a simple link. Control who can view or edit your work.",
+    },
+    {
+      icon: <Layers className="h-10 w-10" />,
+      title: "Infinite canvas",
+      description: "Never run out of space with our infinite canvas that expands as you draw.",
+    }
+  ]
+
   return (
-    <div className="bg-black text-white min-w-screen min-h-screen">
+    <div className="bg-black text-white min-w-screen min-h-screen min-h-full">
 
       <div className="top-0 left-0 border-y border-white/20 bg-black w-screen h-20">
         <div className="flex justify-between items-center h-full px-6">
@@ -20,7 +38,7 @@ export default function Home() {
             <div className="hover:underline hover:underline-offset-3 font-semibold transition-all duration-300 hover:-translate-y-1 cursor-pointer">Testimonials</div>
             <div className="hover:underline hover:underline-offset-3 font-semibold transition-all duration-300 hover:-translate-y-1 cursor-pointer">Pricing</div>
 
-            <div className="flex gap-3">
+            <div className="flex gap-2">
               <Link href="/signup">
                 <button className="bg-white font-semibold text-black transition-all duration-300 border hover:bg-white/80 px-3 py-2 rounded-md cursor-pointer">Signup</button>
               </Link>
@@ -34,7 +52,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex justify-between p-5 pt-12 pl-25 pr-25">
+      <div className="flex justify-between p-5 pt-12 pl-25 pr-25 pb-20 border-b border-white/30">
 
         <div className="flex flex-col pt-5">
           <div className="tracking-wide font-bold text-6xl pb-5">
@@ -66,6 +84,33 @@ export default function Home() {
 
         <Image className="hover:shadow-lg transition-all duration-200 hover:shadow-white/30" src="https://i.pinimg.com/736x/6e/22/33/6e22335dfb94c453afefc69cb46528f2.jpg" alt="image" width={400} height={500} />
       </div>
+
+
+      <div className="pb-30 border-b border-white/30">
+        <h1 className="font-bold text-5xl pt-10 pl-15 mr-18 flex justify-center">Powerful features for creative minds</h1>
+        <p className="mr-18 pt-5 text-gray-400 flex justify-center text-xl text-center">Everything you need to bring your ideas to life, collaborate with others, <br />
+          and share your vision.</p>
+        <div className="flex gap-12 justify-center pt-5">
+
+          {features.map((feature, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 40 }}
+              whileInView={{ opacity: 1, y: 40 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.4, delay: index * 0.2 }}
+              className="bg-black border border-white/20 text-white/90 p-4 w-78 rounded-md transition-all duration-300 hover:shadow-lg hover:shadow-white/10 hover:-translate-y-1"
+            >
+              <div className="w-fit pb-6 p-4">{feature.icon}</div>
+              <h3 className="text-2xl font-bold pb-4">{feature.title}</h3>
+              <p className="text-gray-400 text-wrap">{feature.description}</p>
+
+            </motion.div>
+          ))}
+
+        </div>
+      </div>
+
 
     </div>
   );
