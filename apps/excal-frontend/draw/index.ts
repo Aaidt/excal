11470ef10288cfa3373ffce917b7ec4718aa24c2
Aadit <aadits2004@gmail.com>
2025-06-07 10:03:03 +0000

    persisting existing shapes

--- apps/excal-frontend/draw/index.ts
+++ apps/excal-frontend/draw/index.ts
@@ -1,11 +1,23 @@
-
+type Shapes = {
+    type: "Rect",
+    x: number,
+    y: number,
+    width: number,
+    height: number
+} | {
+    type: "circle",
+    centerX: number,
+    centerY: number,
+    radius: number
+}
 
 export default function initDraw(canvas: HTMLCanvasElement) {
-    
+
     const ctx = canvas.getContext('2d');
-    if (!ctx) {
-        return
-    }
+    if (!ctx) return
+
+    const existingShapes: Shapes[] = []
+
     ctx.fillStyle = "black"
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -21,22 +33,42 @@ export default function initDraw(canvas: HTMLCanvasElement) {
 
     canvas.addEventListener("mouseup", (e: MouseEvent) => {
         clicked = false
-        console.log(e.clientX)
-        console.log(e.clientY)
+
+        const width = e.clientX - startX
+        const height = e.clientY- startY
+        existingShapes.push({
+            type: "Rect",
+            x: startX,
+            y: startY,
+            width,
+            height
+        })
     })
 
     canvas.addEventListener("mousemove", (e: MouseEvent) => {
         if (clicked) {
             const width = e.clientX - startX;
             const height = e.clientY - startY;
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            ctx.fillStyle = "black"
-            ctx.fillRect(0, 0, canvas.width, canvas.height)
+            clearCanvas(existingShapes, canvas, ctx)
 
             ctx.strokeStyle = "white"
             ctx.strokeRect(startX, startY, width, height);
         }
     })
 
+}
+
+function clearCanvas(existingShapes: Shapes[], canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "black"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    existingShapes.map((shape) => {
+        if (shape.type === "Rect") {
+            ctx.strokeStyle = "white"
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    })
 }
